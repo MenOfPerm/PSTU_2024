99aa75c413c180cd274b7fc6837dfd8b31ba6758
van <permuakivan@Gmail.com>
2025-04-08 10:15:47 +0500

    [b[b-[f[f

---
+++ PSTU_2024/sem_2/5/Lab_5(Lab_9).cpp
@@ -0,0 +1,45 @@
+﻿//1) Скопировать в файл F2 только те строки из F1,  
+// которые начинаются с буквы «А».
+//2) Подсчитать количество слов в F2
+
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	string str;
+	ifstream myf1("F1.txt");
+	fstream myf2("F2.txt");
+	if (!myf2.is_open())
+	{
+		cout << "error" << endl;
+	}
+
+	while (getline(myf1, str))
+	{
+		if (str[0] == 'A')
+		{
+			myf2 << str << endl;
+		}
+	}
+	myf1.close();
+	myf2.close();
+	string ch;
+	ifstream myf("F2.txt");
+
+	int k = 0;
+	while (getline(myf, ch))
+	{
+		k++;
+	}
+	myf.close();
+	
+
+	cout << k << endl;
+	return 0;
+}
